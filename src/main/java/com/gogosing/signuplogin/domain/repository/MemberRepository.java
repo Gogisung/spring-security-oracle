d346c83da4b2b1344b0cf0f0d3c600c428d62dd5
Gogisung <ggs0707@naver.com>
2020-11-17 07:07:06 +0900

    first

---
+++ src/main/java/com/gogosing/signuplogin/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.gogosing.signuplogin.domain.repository;
+
+import com.gogosing.signuplogin.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
---
+++ src/test/java/com/gogosing/signuplogin/SignuploginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogosing.signuplogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignuploginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
